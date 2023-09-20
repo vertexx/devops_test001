fc04a040ffe081642254d9274b4af0cac244c64e
Vertexx <underhell@inbox.ru>
2023-09-20 21:31:14 +0800

    Add second VM and IP

--- main.tf
+++ main.tf
@@ -68,6 +68,14 @@ resource "azurerm_public_ip" "devops_test_pub_ip" {
   tags                = var.common_tags
 }
 
+resource "azurerm_public_ip" "devops_test_pub_ip2" {
+  name                = "DevOps-Test-Pub-IP2"
+  resource_group_name = azurerm_resource_group.devops_test_rg.name
+  location            = azurerm_resource_group.devops_test_rg.location
+  allocation_method   = "Static"
+  tags                = var.common_tags
+}
+
 resource "azurerm_network_interface" "devops_test_nic_01" {
   name                = "DevOps-Test-NIC-01"
   resource_group_name = azurerm_resource_group.devops_test_rg.name
@@ -78,6 +86,20 @@ resource "azurerm_network_interface" "devops_test_nic_01" {
     subnet_id                     = azurerm_subnet.devops_test_vn_subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.devops_test_pub_ip.id
+  }
+  tags = var.common_tags
+}
+
+resource "azurerm_network_interface" "devops_test_nic_02" {
+  name                = "DevOps-Test-NIC-02"
+  resource_group_name = azurerm_resource_group.devops_test_rg.name
+  location            = azurerm_resource_group.devops_test_rg.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.devops_test_vn_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.devops_test_pub_ip2.id
   }
   tags = var.common_tags
 }
@@ -120,8 +142,51 @@ resource "azurerm_linux_virtual_machine" "devops_test_vm_01" {
   tags = var.common_tags
 }
 
+resource "azurerm_linux_virtual_machine" "devops_test_vm_02" {
+  name                  = "DevOps-Test-VM-02"
+  resource_group_name   = azurerm_resource_group.devops_test_rg.name
+  location              = azurerm_resource_group.devops_test_rg.location
+  size                  = var.vm_size
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.devops_test_nic_02.id]
+
+  custom_data = filebase64("customdata.tpl")
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("ssh-script-${var.host_os}.tpl", {
+      hostname     = self.public_ip_address,
+      user         = "adminuser",
+      identityfile = "~/.ssh/id_rsa"
+    })
+    interpreter = var.host_os == "windows" ? ["Powershell", "-Command"] : ["bash", "-c"]
+  }
+  tags = var.common_tags
+}
+
 data "azurerm_public_ip" "devops_test_pub_ip_data" {
   name                = azurerm_public_ip.devops_test_pub_ip.name
+  resource_group_name = azurerm_resource_group.devops_test_rg.name
+}
+
+data "azurerm_public_ip" "devops_test_pub_ip2_data" {
+  name                = azurerm_public_ip.devops_test_pub_ip2.name
   resource_group_name = azurerm_resource_group.devops_test_rg.name
 }
 
--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "public_ip_address" {
   value = "${azurerm_linux_virtual_machine.devops_test_vm_01.name}: ${data.azurerm_public_ip.devops_test_pub_ip_data.ip_address}"
 }
 
+output "public_ip_address2" {
+  value = "${azurerm_linux_virtual_machine.devops_test_vm_02.name}: ${data.azurerm_public_ip.devops_test_pub_ip2_data.ip_address}"
+}
+
 output "my_public_ip" {
   value = data.http.my_public_ip.response_body
 }
