bfe37a3e69da004c941c1dfd250eb66cab87b790
Vertexx <underhell@inbox.ru>
2023-10-03 22:19:45 +0800

    Edit outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "public_ip_address" {
+output "public_ip_address1" {
   value = "${azurerm_linux_virtual_machine.devops_test_vm_01.name}: ${data.azurerm_public_ip.devops_test_pub_ip_data.ip_address}"
 }
 
